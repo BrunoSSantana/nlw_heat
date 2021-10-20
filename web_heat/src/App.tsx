11c397037f85a83128c2ba084f5e262dd65b7848
BrunoSSantana <brunoosouza15@gmail.com>
2021-10-20 13:00:09 -0300

    feature: [WEB] implement AuthContext

--- web_heat/src/App.tsx
+++ web_heat/src/App.tsx
@@ -1,12 +1,18 @@
-import style from './App.module.scss'
+import { useContext } from 'react'
+import styles from './App.module.scss'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
+import { AuthContext } from './contexts/auth'
 
 export function App() {
+
+  const { user } = useContext(AuthContext)
+
   return (
-    <main className={style.contentWrapper}>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
       <MessageList />
-      <LoginBox />
+      { !!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   )
 }
